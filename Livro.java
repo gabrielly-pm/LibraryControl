e955fc5dde6393969ed4ed85a2f2f8d303661984
Gabrielly <gaby.20pereiramachado@gmail.com>
2026-05-05 21:16:26 -0300

    adicionar controle de disponibilidade

--- Livro.java
+++ Livro.java
@@ -3,11 +3,13 @@ public class Livro {
     String titulo;
     String autor;
     int paginas;
+    boolean disponivel;
 
     public Livro(String titulo, String autor, int paginas) {
         this.titulo = titulo;
         this.autor = autor;
         this.paginas = paginas;
+        this.disponivel = true;
     }
 
     public void exibirInformacoes() {
